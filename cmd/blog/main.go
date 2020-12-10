b181665c1275f8bc2555e1e028059ea47e4536ff
luca <beccibrian@gmail.com>
2020-12-10 03:55:13 -0300

    feat: server configuration and env config added

--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/signal"
+
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/lucabecci/go-api-blog/internal/server"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 
+	serv, err := server.New(port)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go serv.Start()
+
+	command := make(chan os.Signal, 1)
+	signal.Notify(command, os.Interrupt)
+	<-command
+
+	serv.Close()
 }
